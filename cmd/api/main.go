b1deb790dfe1eae2f6bd4d277ba9d28bca7dbadd
Vyacheslav Pokosa <pokosa.v@wismolabs.com>
2026-02-22 19:28:22 +0100

    feat: Implement initial API server with a health endpoint and update `.env-example` to include host configurations for services.

---
+++ cmd/api/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
+func main() {
+	_ = godotenv.Load()
+
+	dbHost := getEnv("POSTGRES_HOST", "localhost")
+	dbPort := getEnv("POSTGRES_PORT", "5432")
+	dbUser := getEnv("POSTGRES_USER", "postgres")
+	dbPass := getEnv("POSTGRES_PASSWORD", "postgres")
+	dbName := getEnv("POSTGRES_DB", "qantlo")
+
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		dbUser, dbPass, dbHost, dbPort, dbName)
+	
+	fmt.Println(dsn)
+
+	port := getEnv("API_PORT", "8080")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Qantlo Engine is running!"))
+	})
+
+	fmt.Printf("🟢 Server is running on port :%s\n", port)
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
+
