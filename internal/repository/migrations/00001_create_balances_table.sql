96af1948c135ddb1401eabc10dbe2547dc935602
Vyacheslav Pokosa <pokosa.v@wismolabs.com>
2026-02-23 23:56:11 +0100

    feat: add initial balances table migration

--- internal/repository/migrations/00001_create_balances_table.sql
+++ internal/repository/migrations/00001_create_balances_table.sql
@@ -4,8 +4,11 @@ CREATE TABLE balances (
     resource_type VARCHAR(50)  NOT NULL,
     amount        BIGINT       NOT NULL DEFAULT 0,
     updated_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at    TIMESTAMP WITH TIME ZONE DEFAULT NULL,
     PRIMARY KEY (account_id, resource_type)
 );
+CREATE INDEX idx_balances_deleted_at ON balances (deleted_at);
+
 
 -- +goose Down
 DROP TABLE balances;
